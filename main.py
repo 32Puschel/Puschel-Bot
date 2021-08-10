0dc499e0809563d8d19e5f8f4c5016c2ee8e298b
Puschel <68943564+32Puschel@users.noreply.github.com>
2021-08-10 23:22:10 +0200

    Update and rename the code idk.py to main.py

--- main.py
+++ main.py
@@ -15,28 +15,18 @@ async def on_member_join(member):
 @client.event
 async def on_member_remove(member):
     print(f'{member} has left a Server.')
-    
-@client.event
-async def on_message(message):
 
-	if message.content == '.test':
-		await message.channel.send('No u')
-       
-@client.event
-async def on_message(message):
-
-	if message.content == '.help':
-		await message.channel.send('U aint getting any help from me.')
 
 @client.event
 async def on_message(message):
 
-	if message.content == '.dorto':
+	if message.content == 'p!oghelp':
+		await message.channel.send('U ain\'t gettin any help from me ;)')
+	if message.content == 'p!dorto':
 		await message.channel.send('https://cdn.discordapp.com/emojis/870388745554231306.png?v=1')
-
-@client.command()
-@commands.is_owner()
-async def shutdown(ctx):
-    await ctx.bot.logout()
+	if message.content == 'p!test':
+		await message.channel.send('No u')
+	if message.content == 'p!help':
+		await message.channel.send('No u') #gotta figure out how embeds work first
 
 client.run('your Bot token')
